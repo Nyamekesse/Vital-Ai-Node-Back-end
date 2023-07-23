8b9c7428494bd1a127e8f03f94ef49947f572bbb
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-07-23 15:28:24 +0000

    added dependencies, configured server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("sup");
+import express from "express";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors({ credentials: true }));
+
+app.use(compression());
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
