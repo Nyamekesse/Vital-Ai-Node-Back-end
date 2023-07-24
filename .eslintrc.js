208f2f42d772efb2747d80ea71c1f68f06781af2
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-07-24 19:44:26 +0000

    added and setup eslint, airbnb guid and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+export default {
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
