ab45c090d43707ab6907f1eb3bdf169ccf8529f8
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-07-28 20:57:01 +0000

    refactored code

--- src/controllers/health-professionals.ts
+++ src/controllers/health-professionals.ts
@@ -2,54 +2,60 @@ import { HealthProfessional, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import validator from "validator";
 
-
 const prisma = new PrismaClient();
 
-export const fillProfileHealthProfessional = async (req: Request, res: Response) => {
-  const userId = req.user!.id
-   const userType = req.user!.userType
-   const userInput: HealthProfessional = req.body;
-  
-   try {
+export const fillProfileHealthProfessional = async (
+  req: Request,
+  res: Response
+) => {
+  const userId = req.user!.id;
+  const userInput: HealthProfessional = req.body;
+
+  try {
     if (!validator.isLength(userInput.firstName, { min: 1, max: 255 })) {
-      return res.status(400).json({message:"First name must be between 1 and 255 characters"});
+      return res
+        .status(400)
+        .json({ message: "First name must be between 1 and 255 characters" });
     }
 
     if (!validator.isLength(userInput.lastName, { min: 1, max: 255 })) {
-      return res.status(400).json({message:"Last name must be between 1 and 255 characters"});
+      return res
+        .status(400)
+        .json({ message: "Last name must be between 1 and 255 characters" });
     }
-    if (!validator.isLength(userInput.specialization, { min: 1, max: 255 })) {
-      return res.status(400).json({message:"Enter correct specialization"});
+    if (!validator.isLength(userInput.specializationId, { min: 1, max: 255 })) {
+      return res.status(400).json({ message: "Enter correct specialization" });
     }
-    if (!validator.isLength(userInput.medicalLicenseNumber, { min: 1, max: 30 })) {
-      return res.status(400).json({message:"Invalid medical License"});
+    if (
+      !validator.isLength(userInput.medicalLicenseNumber, { min: 1, max: 30 })
+    ) {
+      return res.status(400).json({ message: "Invalid medical License" });
     }
-    if (!validator.isUUID(userInput.organizationID,)) {
-      return res.status(400).json({message:"Invalid Organization ID"});
+    if (!validator.isUUID(userInput.organizationID)) {
+      return res.status(400).json({ message: "Invalid Organization ID" });
     }
     if (!validator.isMobilePhone(userInput.contactInfo)) {
-      return res.status(400).json({message:"Contact info must be a valid phone number"});
-    }
-    if (!validator.isURL(userInput.profilePicture)) {
-      return res.status(400).json({message:"Contact info must be a valid phone number"});
+      return res
+        .status(400)
+        .json({ message: "Contact info must be a valid phone number" });
     }
 
     const user = await prisma.healthProfessional.create({
       data: {
         firstName: userInput.firstName,
         lastName: userInput.lastName,
-        specialization:'',
-        medicalLicenseNumber:"",
+        specializationId: userInput.specializationId,
+        medicalLicenseNumber: userInput.medicalLicenseNumber,
         contactInfo: userInput.contactInfo,
-        userId:"4a5d0353-8c4a-4ba2-b37c-acacde2b936c",
-        organizationID:"d8912da1-4073-4e41-b6b6-33b77e39fe28",
-        profilePicture:'https://i.pinimg.com/originals/07/33/ba/0733ba760b29378474dea0fdbcb97107.png'
+        userId,
+        organizationID: userInput.organizationID,
+        profilePicture: `https://api.multiavatar.com/${userId}.svg?apikey=${process.env.MULTI_AVATAR_API_KEY}`,
       },
     });
 
     return res.status(201).json({ user });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({message:"Failed to create user"})
+    return res.status(500).json({ message: "Failed to create user" });
   }
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import routes from "./routes/index.js";
+import { nanoid } from "nanoid";
 
+const id = nanoid();
+console.log(id);
 const secret = process.env.SECRET;
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3030;
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(compression());
@@ -15,6 +18,8 @@ app.use(cookieParser(secret));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Vital Ai server listening on port ${PORT}`)
+);
 
 app.use("/", routes());
