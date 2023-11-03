75a3bfe74a567b548f89d24f608ec396cc0ea9cd
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-11-03 16:22:28 +0000

    refactored code

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -106,7 +106,7 @@ export const login = async (req: Request, res: Response) => {
     const token = jwt.sign(payload, secret);
 
     res.cookie("vital_ai_token", token, {
-      domain: "https://frontend-test-j1xl.onrender.com",
+      domain: "frontend-test-j1xl.onrender.com",
       httpOnly: false,
       signed: true,
       secure: process.env.NODE_ENV === "production",
