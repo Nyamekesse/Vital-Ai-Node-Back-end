1598089eeaa0015609056005f5d58eeb7de945e8
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-11-22 12:27:09 +0000

    updated server uptime controller for testing

--- src/controllers/serverUptimeController.ts
+++ src/controllers/serverUptimeController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
 export const getUptimeStatus = async (req: Request, res: Response) => {
+  console.log(process.env.NODE_ENV === "production");
   return res.status(200).json({ message: "Server Up and running" });
 };
